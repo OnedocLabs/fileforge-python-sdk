c2e977bd18e3d2e70144e8e803d7b74d5b46d653
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-05-26 10:39:40 +0000

    Release 0.1.0

--- src/fileforge/core/client_wrapper.py
+++ src/fileforge/core/client_wrapper.py
@@ -27,7 +27,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "fileforge",
-            "X-Fern-SDK-Version": "0.0.13",
+            "X-Fern-SDK-Version": "0.1.0",
         }
         username = self._get_username()
         password = self._get_password()
